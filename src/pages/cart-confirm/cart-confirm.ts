0fd153c2cc3627ee92b890d8eaec0a472c7db9ac
Khanh Luong <khanhld1910@gmail.com>
2018-03-13 02:09:32 +0700

    updated

--- src/pages/cart-confirm/cart-confirm.ts
+++ src/pages/cart-confirm/cart-confirm.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angu
 import { SingleBill } from '../../interfaces/bill';
 import { UserDataProvider } from '../../providers/user-data';
 import { AddressModalPage } from '../address-modal/address-modal';
+import { MyDbProvider } from '../../providers/my-db';
+import { MyToastProvider } from '../../providers/my-toast';
 
 
 @IonicPage({
@@ -17,12 +19,19 @@ export class CartConfirmPage {
   cartBills: SingleBill[]
   addresses: string[] = []
   addressSelectedIndex: number = 0
+  time: string
+  timeDeliver: string
+
+  min: string
+  max: string
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private userData: UserDataProvider,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private myDBProvider: MyDbProvider,
+    private myToasProvider: MyToastProvider
   ) {
     this.cartBills = this.navParams.get('cart')
   }
@@ -31,6 +40,39 @@ export class CartConfirmPage {
     this.userData
       .getUserAddresses(this.userData.userPhone)
       .subscribe(value => this.addresses = value)
+
+
+    this.setTimeRange()
+    this.time = this.min
+  }
+
+  setTimeRange() {
+    let now = new Date()
+    let nowUnix = now.getTime()
+
+    let tomorrowUnix = nowUnix + 1 * 24 * 60 * 60 * 1000
+    let tomorrow = new Date(tomorrowUnix)
+
+    let minUnix = nowUnix + 5 * 60 * 60 * 1000 // for 5 hours after this time
+    let maxUnix = nowUnix + 7 * 24 * 60 * 60 * 1000 // for 7 days after this time
+
+    let min = new Date(minUnix)
+    let max = new Date(maxUnix)
+
+    if (now.getHours() < 11) {
+      this.min = `${min.getFullYear()}-${this.userData.lessThan10Format(min.getMonth() + 1)}-${this.userData.lessThan10Format(min.getDate())}`
+      this.timeDeliver = (now.getHours() < 8) ? 'morning' : 'afternoon'
+    } else {
+      this.min = `${min.getFullYear()}-${this.userData.lessThan10Format(tomorrow.getMonth() + 1)}-${this.userData.lessThan10Format(tomorrow.getDate())}`
+      this.timeDeliver = 'morning'
+    }
+    this.max = `${max.getFullYear()}-${this.userData.lessThan10Format(max.getMonth() + 1)}-${this.userData.lessThan10Format(max.getDate())}`
+
+  }
+
+  sentTime() {
+    let now = new Date()
+    return `${now.getFullYear()}-${this.userData.lessThan10Format(now.getMonth() + 1)}-${this.userData.lessThan10Format(now.getDate())}T${this.userData.lessThan10Format(now.getHours())}:${this.userData.lessThan10Format(now.getMinutes())}`
   }
 
   isAddressChecked(addressIndex: number) {
@@ -56,6 +98,46 @@ export class CartConfirmPage {
     }
 
     addressModal.present()
+  }
+
+
+  confirmInvoice() {
+
+
+    let reqBills = []
+
+    for (let i = 0; i < this.cartBills.length; i++) {
+      if (this.cartBills[i].quantity > 0) {
+        reqBills.push(this.cartBills[i])
+      }
+    }
+
+    let deliverTime = (this.timeDeliver == 'morning') ? `${this.time}T08:00` : `${this.time}T14:00`
+    //console.log(this.timeDeliver, deliverTime)    
+
+    this
+      .myDBProvider
+      .sentReqFromCart(
+        reqBills,
+        this.userData.userPhone,
+        deliverTime,
+        this.sentTime(),
+        this.navParams.get('total'),
+        this.addresses[this.addressSelectedIndex]
+      )
+      .subscribe(success => {
+        this.myToasProvider
+          .myToast({
+            message: 'Yêu cầu đặt hàng thành công!',
+            duration: 1500,
+            position: 'top',
+            cssClass: 'toast-info'
+          }, () => {
+            this.navCtrl.setRoot('TabsPage', {index: 3})
+          })
+      })
+      
+
   }
 
 }
--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -47,95 +47,12 @@ export class CartPage {
         this.allProducts = value
       })
 
-    this.userData
-      .getUserInfo(this.userData.userPhone)
-      .first()
-      .subscribe(info => {
-        this.userInfo = info
-        this.req.address = this.userInfo.address[.0] || null
-      })
-
-    this.setTimeRange()
-    this.time = this.min
   }
 
-  min: string
-  max: string
-  setTimeRange() {
-    let now = new Date()
-    let nowUnix = now.getTime()
-
-    let tomorrowUnix = nowUnix + 1 * 24 * 60 * 60 * 1000
-    let tomorrow = new Date(tomorrowUnix)
-
-    let minUnix = nowUnix + 5 * 60 * 60 * 1000 // for 5 hours after this time
-    let maxUnix = nowUnix + 7 * 24 * 60 * 60 * 1000 // for 7 days after this time
-
-    let min = new Date(minUnix)
-    let max = new Date(maxUnix)
-
-    if (now.getHours() < 11) {
-      this.min = `${min.getFullYear()}-${this.userData.lessThan10Format(min.getMonth() + 1)}-${this.userData.lessThan10Format(min.getDate())}T14:00`
-    } else {
-      this.min = `${min.getFullYear()}-${this.userData.lessThan10Format(tomorrow.getMonth() + 1)}-${this.userData.lessThan10Format(tomorrow.getDate())}T08:00`
-    }
-    this.max = `${max.getFullYear()}-${this.userData.lessThan10Format(max.getMonth() + 1)}-${this.userData.lessThan10Format(max.getDate())}`
-
-  }
-
-  sentTime() {
-    let now = new Date()
-    return `${now.getFullYear()}-${this.userData.lessThan10Format(now.getMonth() + 1)}-${this.userData.lessThan10Format(now.getDate())}T${this.userData.lessThan10Format(now.getHours())}:${this.userData.lessThan10Format(now.getMinutes())}`
-  }
 
   removeBill(bill: SingleBill) {
     this.myDBProvider
       .removeCardBill(bill)
-  }
-
-  confirmInvoice(form: NgForm) {
-    this.submitted = true
-    if (!form.valid) return
-
-    let reqBills = []
-
-    for (let i = 0; i < this.cartBills.length; i++) {
-      if (this.cartBills[i].quantity > 0) {
-        reqBills.push(this.cartBills[i])
-      }
-    }
-
-    if (!this.userInfo.address) {
-      // update address to profile
-      this.myDBProvider.updateUserInfo({
-        phone: this.userData.userPhone,
-        address: [this.req.address]
-      })
-      console.log(this.req.address)
-    }
-
-    this
-      .myDBProvider
-      .sentReqFromCart(
-        reqBills,
-        this.userData.userPhone,
-        this.time,
-        this.sentTime(),
-        this.getTotalCost(),
-        this.req.address
-      )
-      .subscribe(success => {
-        this.myToasProvider
-          .myToast({
-            message: 'Yêu cầu đặt hàng thành công!',
-            duration: 1500,
-            position: 'top',
-            cssClass: 'toast-info'
-          }, () => {
-            this.cartBills = []
-          })
-      })
-
   }
 
   getTotalCost() {
@@ -181,7 +98,7 @@ export class CartPage {
       })
       return
     }
-    this.navCtrl.push('CartConfirmPage', {'cart': this.cartBills})
+    this.navCtrl.push('CartConfirmPage', {'cart': this.cartBills, 'total': this.getTotalCost()})
   }
 
 }
